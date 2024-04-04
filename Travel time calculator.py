860f963828b09e3df1dc7b830bcd11df299dc157
DaniellaSiqueira <daniella.siqueira.psn@gmail.com>
2024-04-03 23:09:54 -0300

    nine commit

---
+++ Travel time calculator.py
@@ -0,0 +1,5 @@
+distancia = int(input ('Qual a distância a ser percorrida?: '))
+velocidade = int(input ('Qual a velocidade média?: '))
+distancia_velocidade = distancia / velocidade
+tempo = distancia_velocidade
+print (f' O tempo de viagem vai ser de aproximadamente: {tempo}')
